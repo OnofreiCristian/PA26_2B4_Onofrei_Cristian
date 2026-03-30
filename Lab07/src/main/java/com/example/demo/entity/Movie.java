78c813e44c636e6beeae593937426f50e98baf34
Cristian Onofrei <cristianonofrei01@gmail.com>
2026-03-30 22:18:16 +0300

    Added Lab07 Compulsory

---
+++ Lab07/src/main/java/com/example/demo/entity/Movie.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    private Integer id;
+    private String title;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    private Integer duration;
+
+    private Float score;
+
+    @Column(name = "genre_id")
+    private Integer genreId;
+
+}
